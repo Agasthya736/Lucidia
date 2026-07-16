b52f721f6012fc04c34b132cf35cfbacfe24946e
VeriRad Dev <dev@verirad.local>
2026-07-16 15:33:43 +0530

    Project Initialized

---
+++ backend/src/main/java/com/example/Lucidia/LucidiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lucidia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucidiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucidiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Lucidia/LucidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lucidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
